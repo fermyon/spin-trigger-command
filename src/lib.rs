8b70a30f8da350299d800ec4d3c2d7f8d013aac9
karthik2804 <karthik.ganeshram@fermyon.com>
2024-03-26 11:51:35 -0700

    add support for preview 2 components that target the wasi:cli world
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,25 @@
-use std::sync::Arc;
-
 use anyhow::{Context, Result};
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use spin_app::AppComponent;
 use spin_core::Engine;
-use spin_trigger::{
-    cli::NoArgs, EitherInstance, EitherInstancePre, TriggerAppEngine, TriggerExecutor,
-};
+use spin_trigger::TriggerInstancePre;
+use spin_trigger::{cli::NoArgs, TriggerAppEngine, TriggerExecutor};
 
 pub(crate) type RuntimeData = ();
+pub(crate) type Store = spin_core::Store<RuntimeData>;
 
 pub struct CommandTrigger {
     engine: TriggerAppEngine<Self>,
     components: Vec<Component>,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 pub struct Component {
     pub id: String,
+    #[serde(default)]
+    pub executor: CommandExecutorType,
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
@@ -28,9 +29,29 @@ struct TriggerMetadata {
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(deny_unknown_fields, rename_all = "lowercase", tag = "type")]
+pub enum CommandExecutorType {
+    #[default]
+    Preview2,
+    Preview1,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct CommandTriggerConfig {
     pub component: String,
+    #[serde(default)]
+    pub executor: CommandExecutorType,
+}
+
+pub enum CommandInstancePre {
+    Component(spin_core::InstancePre<RuntimeData>),
+    Module(spin_core::ModuleInstancePre<RuntimeData>),
+}
+
+pub enum CommandInstance {
+    Component(spin_core::Instance),
+    Module(spin_core::ModuleInstance),
 }
 
 #[async_trait]
@@ -39,74 +60,93 @@ impl TriggerExecutor for CommandTrigger {
     type RuntimeData = RuntimeData;
     type TriggerConfig = CommandTriggerConfig;
     type RunConfig = NoArgs;
+    type InstancePre = CommandInstancePre;
 
     async fn new(engine: TriggerAppEngine<Self>) -> Result<Self> {
         let components = engine
             .trigger_configs()
             .map(|(_, config)| Component {
                 id: config.component.clone(),
+                executor: config.executor.clone(),
             })
             .collect();
         Ok(Self { engine, components })
     }
 
     async fn run(self, _config: Self::RunConfig) -> Result<()> {
-        let components = self.components;
-        let engine = Arc::new(self.engine);
-
-        // TODO:
-        // This only uses first component for now.
-        let executor = Executor::new(engine, components[0].clone());
-
-        executor.run().await
+        self.handle().await
     }
+}
 
-    // TODO:
-    // This function assumes WASI P1
+#[async_trait]
+impl TriggerInstancePre<RuntimeData, CommandTriggerConfig> for CommandInstancePre {
+    type Instance = CommandInstance;
+
     async fn instantiate_pre(
-        engine: &Engine<Self::RuntimeData>,
+        engine: &Engine<RuntimeData>,
         component: &AppComponent,
-        _config: &Self::TriggerConfig,
-    ) -> Result<EitherInstancePre<Self::RuntimeData>> {
-        let module = component.load_module(engine).await?;
-        Ok(EitherInstancePre::Module(
-            engine
-                .module_instantiate_pre(&module)
-                .with_context(|| format!("Failed to instantiate component '{}'", component.id()))?,
-        ))
+        config: &CommandTriggerConfig,
+    ) -> Result<CommandInstancePre> {
+        if let CommandExecutorType::Preview1 = &config.executor {
+            let module = component.load_module(engine).await?;
+            Ok(CommandInstancePre::Module(
+                engine.module_instantiate_pre(&module)?,
+            ))
+        } else {
+            let comp = component.load_component(engine).await?;
+            Ok(CommandInstancePre::Component(
+                engine.instantiate_pre(&comp)?,
+            ))
+        }
+    }
+
+    async fn instantiate(&self, store: &mut Store) -> Result<CommandInstance> {
+        match self {
+            CommandInstancePre::Component(pre) => pre
+                .instantiate_async(store)
+                .await
+                .map(CommandInstance::Component),
+            CommandInstancePre::Module(pre) => pre
+                .instantiate_async(store)
+                .await
+                .map(CommandInstance::Module),
+        }
     }
 }
 
-pub struct Executor {
-    pub engine: Arc<TriggerAppEngine<CommandTrigger>>,
-    pub component: Component,
-}
+impl CommandTrigger {
+    pub async fn handle(&self) -> Result<()> {
+        let component = &self.components[0];
+        match component.executor {
+            CommandExecutorType::Preview2 => {
+                let (instance, mut store) = self.engine.prepare_instance(&component.id).await?;
+                let CommandInstance::Component(instance) = instance else {
+                    unreachable!()
+                };
+                let handler =
+                    wasmtime_wasi::preview2::command::Command::new(&mut store, &instance)?;
+                let _ = handler.wasi_cli_run().call_run(store).await?;
+            }
+            CommandExecutorType::Preview1 => {
+                let store_builder = self
+                    .engine
+                    .store_builder(&component.id, spin_core::WasiVersion::Preview1)?;
+                let (instance, mut store) = self
+                    .engine
+                    .prepare_instance_with_store(&component.id, store_builder)
+                    .await?;
 
-impl Executor {
-    pub fn new(engine: Arc<TriggerAppEngine<CommandTrigger>>, component: Component) -> Self {
-        Self { engine, component }
-    }
+                let CommandInstance::Module(instance) = instance else {
+                    unreachable!()
+                };
 
-    pub async fn run(&self) -> Result<()> {
-        let store_builder = self
-            .engine
-            .store_builder(&self.component.id, spin_core::WasiVersion::Preview1)?;
-        let (instance, mut store) = self
-            .engine
-            .prepare_instance_with_store(&self.component.id, store_builder)
-            .await?;
+                let start = instance
+                    .get_func(&mut store, "_start")
+                    .context("Expected component to export _start function")?;
 
-        let EitherInstance::Module(instance) = instance else {
-            unreachable!()
+                let _ = start.call_async(&mut store, &[], &mut []).await?;
+            }
         };
-
-        let start = instance
-            .get_func(&mut store, "")
-            .or_else(|| instance.get_func(&mut store, "_start"))
-            .context("Expected component to export _start function")?;
-
-        let _ = start.call_async(&mut store, &[], &mut []).await?;
-
         Ok(())
     }
 }
